8036a3be87aec41802ab0d0e25b24b2cff485842
Franz Becker <franz.becker@signal-iduna.de>
2017-07-11 13:34:25 +0200

    fix angular integration and add launch config

--- src/assets/main.js
+++ src/assets/main.js
@@ -13,39 +13,44 @@ require.config({
     }
 });
 
+/* Setup ajax / xtext services. */
+require(
+    ["jquery", "ace/ace", "xtext/xtext-ace", "xtext/services/XtextService"],
+    function (jquery, ace, xtext, xtextService) {
+        ace.config.set('basePath', baseUrl + 'assets/ace');
+
+        /* Send the session credentials also cross-domain. */
+        jquery.ajaxSetup({
+            xhrFields: {
+                withCredentials: true
+            }
+        });
+
+        /* Dummy authorization for now... */
+        xtextService.prototype._sendRequest = xtextService.prototype.sendRequest;
+        xtextService.prototype.sendRequest = function (editorContext, settings, needsSession) {
+            settings.headers = {
+                "Authorization": "admin:admin@example.org"
+            };
+            this._sendRequest(editorContext, settings, needsSession);
+        };
+    }
+);
+
 function createXtextEditor(parent, resourceId) {
     console.log(`createXtextEditor(${parent}, ${resourceId})`);
-    require(["jquery",
-             "ace/ace",
-             "xtext/xtext-ace",
-             "xtext/services/XtextService" ],
-            function(jquery, ace, xtext, xtextService) {
-                ace.config.set('basePath', baseUrl + 'assets/ace');
-
-                /* Send the session credentials also cross-domain. */
-                jquery.ajaxSetup({
-                    xhrFields: {
-                        withCredentials: true
-                    }
-                });
-
-                /* Dummy authorization for now... */
-                var originalSendRequest = xtextService.prototype.sendMyRequest;
-                var newSendRequest = function(editorContext, settings, needsSession) {
-                    settings.headers = {
-                        "Authorization": "admin:admin@example.org"
-                    };
-                    originalSendRequest(editorContext, settings, needsSession); // is bound to original function
-                };
-                xtextService.prototype.sendMyRequest = newSendRequest;
-
-                xtext.createEditor({
-                    baseUrl: baseUrl,
-                    serviceUrl: 'http://localhost:8080/xtext-service',
-                    parent: parent,
-                    resourceId: resourceId,
-                    syntaxDefinition: "xtext-resources/generated/mode-mydsl",
-                    enableSaveAction: true
-                });
+    require(
+        ["jquery", "ace/ace", "xtext/xtext-ace", "xtext/services/XtextService"],
+        function (jquery, ace, xtext, xtextService) {
+            xtext.createEditor({
+                baseUrl: baseUrl,
+                serviceUrl: 'http://localhost:8080/xtext-service',
+                parent: parent,
+                resourceId: resourceId,
+                sendFullText: true,
+                syntaxDefinition: "xtext-resources/generated/mode-mydsl",
+                enableSaveAction: true
             });
+        }
+    );
 }
--- src/assets/xtext-resources/generated/mode-mydsl.js
+++ src/assets/xtext-resources/generated/mode-mydsl.js
@@ -1,18 +1,9 @@
 define(["ace/lib/oop", "ace/mode/text", "ace/mode/text_highlight_rules"], function(oop, mText, mTextHighlightRules) {
 	var HighlightRules = function() {
-		var keywords = "Hello";
+		var keywords = "package";
 		this.$rules = {
 			"start": [
-				{token: "comment", regex: "\\/\\/.*$"},
-				{token: "comment", regex: "\\/\\*", next : "comment"},
-				{token: "string", regex: '["](?:(?:\\\\.)|(?:[^"\\\\]))*?["]'},
-				{token: "string", regex: "['](?:(?:\\\\.)|(?:[^'\\\\]))*?[']"},
-				{token: "constant.numeric", regex: "[+-]?\\d+(?:(?:\\.\\d*)?(?:[eE][+-]?\\d+)?)?\\b"},
 				{token: "keyword", regex: "\\b(?:" + keywords + ")\\b"}
-			],
-			"comment": [
-				{token: "comment", regex: ".*?\\*\\/", next : "start"},
-				{token: "comment", regex: ".+"}
 			]
 		};
 	};
@@ -22,7 +13,7 @@ define(["ace/lib/oop", "ace/mode/text", "ace/mode/text_highlight_rules"], functi
 		this.HighlightRules = HighlightRules;
 	};
 	oop.inherits(Mode, mText.Mode);
-	Mode.prototype.$id = "xtext/mydsl";
+	Mode.prototype.$id = "xtext/tsl";
 	Mode.prototype.getCompletions = function(state, session, pos, prefix) {
 		return [];
 	}
