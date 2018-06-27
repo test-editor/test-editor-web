4550c7860145ef7ebbe7ca1f6e13c7fdb03e621e
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-06-27 09:05:26 +0200

    fix yarn update script

--- yarn-upgrade-testeditor-deps.sh
+++ yarn-upgrade-testeditor-deps.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yarn list --pattern "testeditor.*" --flat | grep '@' | awk  '{ print $2; }' | sed -e "s/@[^@]*$//g" | xargs -L100 -t yarn upgrade
+yarn list | grep '@testeditor' | sed -e "s/^[^@]*//g" | sed -e "s/@[^@]*$//g" | xargs -L100 -t yarn upgrade
