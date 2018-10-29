7ea38b1d6b41509bf1e5d7c7e9c344c034950ec4
Gunther Bachmann <gunther.bachmann@itemis.de>
2018-10-29 14:25:21 +0100

    remove caching, enable e2e test, ensure folder before linking

--- shell-latest.nix
+++ shell-latest.nix
@@ -34,9 +34,8 @@ stdenv.mkDerivation {
     ];
     shellHook = ''
         # make sure no output is done, since direnv fails with direnv: error unmarshal() base64 decoding: illegal base64 data at input byte ?
-
         # get a symbolic link to google-chrome-stable such that the karma runner finds the chrome executable (it does not accept google-chrome-stable itself)
-        ln -sf $(which google-chrome-stable) `pwd`/node_modules/.bin/google-chrome
+        mkdir -p `pwd`/node_modules/.bin && ln -sf $(which google-chrome-stable) `pwd`/node_modules/.bin/google-chrome
         yarn install @angular/cli > /dev/null 2>&1
         # put all linked executables in node_modules on the path
         export PATH=`pwd`/node_modules/.bin:$PATH
