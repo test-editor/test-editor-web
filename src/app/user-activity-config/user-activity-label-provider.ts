2a60313e20f11bc07caf56755c81519954af57b8
Jan Jelschen <jan.jelschen@itemis.de>
2018-12-19 09:50:23 +0100

    configure user activity "opened file" (#140)
    * configure user activity "opened file"
    
    * fix linting error

---
+++ src/app/user-activity-config/user-activity-label-provider.ts
@@ -0,0 +1,30 @@
+import { UserActivityType } from './user-activity-config';
+import { LabelProvider } from '@testeditor/testeditor-commons';
+import { UserActivityLabelSubject } from '@testeditor/test-navigator';
+
+export class TestEditorUserActivityLabelProvider extends LabelProvider<UserActivityLabelSubject> {
+  getLabel(subject: UserActivityLabelSubject): string {
+    return this.userString(subject.users) + ' ' + this.activityString(subject.activityType,
+      subject.users.length < 2, subject.forChildElement);
+  }
+
+  private userString(users: string[]): string {
+    switch (users.length) {
+      case 0: return 'Somebody';
+      case 1: return users[0];
+      case 2: return users[0] + ' and ' + users[1];
+      default: return users.slice(0, -1).join(', ') + ', and ' + users.slice(-1);
+    }
+  }
+
+  private activityString(type: string, singular: boolean, forChildElement: boolean) {
+    switch (type) {
+      case UserActivityType.EXECUTED_TEST:
+        return `${singular ? 'is' : 'are'} executing ${forChildElement ? 'a test in this folder' : 'this test'}`;
+      case UserActivityType.OPENED_FILE:
+        return `${singular ? 'has' : 'have'} opened ${forChildElement ? 'files in this folder' : 'this file'}`;
+      default: return `${singular ? 'is' : 'are'} working on this`;
+    }
+  }
+
+}
