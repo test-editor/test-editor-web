c9497b36682e50626e2f35760bdde88a36843bbb
Jan Jelschen <jan.jelschen@itemis.de>
2019-08-13 18:55:46 +0200

    adapt docker compose to use separate testexec backend (#164)

--- src/assets/configuration.js
+++ src/assets/configuration.js
@@ -3,8 +3,8 @@ var appConfig = function() {
   serviceUrls: {
     xtextService: "http://localhost:8080/xtext-service",
     persistenceService: "http://localhost:9080",
-    testExecutionService: "http://localhost:9080/tests",
-    testSuiteExecutionService: "http://localhost:9080/test-suite",
+    testExecutionService: "http://localhost:10080/tests",
+    testSuiteExecutionService: "http://localhost:10080/test-suite",
     validationMarkerService: "http://localhost:8080/validation-markers",
     indexService: "http://localhost:8080/index",
     testCaseService: "http://localhost:8080/test-case",
